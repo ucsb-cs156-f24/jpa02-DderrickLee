23f7dce4628c43d0e7650ae9d60453e070a3822c
Derrick Lee <d_d_lee@ucsb.edu>
2024-10-09 15:24:28 -0700

    Updated TeamTest.java

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -5,6 +5,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import ch.qos.logback.core.joran.sanity.Pair;
+
+import java.util.ArrayList; 
+
 public class TeamTest {
 
     Team team;
@@ -19,8 +23,62 @@ public class TeamTest {
        assert(team.getName().equals("test-team"));
     }
 
-   
-    // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
-    // 100% mutation coverage (all mutants timed out or killed)
+    @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
 
+    //same object
+    @Test 
+    public void equalsCase1() {
+        Team temp = new Team("temp-team");
+        assert(team.equals(team));
+        assert(!(team.equals(temp)));
+    }
+
+    @Test 
+    public void equalsCase2() {
+        String test = "Different Class";
+        assert(!(team.equals(test)));
+    }
+
+    @Test 
+    public void equalsCase3() {
+        Team t1 = new Team();
+        t1.setName("foo");
+        t1.addMember("bar");
+        Team t2 = new Team();
+        t2.setName("foo");
+        t2.addMember("bar");
+
+        assert(t1.equals(t2));
+
+        t2.setName("chris");
+        assert(!(t1.equals(t2)));
+
+        t2.addMember("john");
+        assert(!(t1.equals(t2)));
+
+        t2.setName("foo");
+        assert(!(t1.equals(t2)));
+    }
+
+    @Test
+    public void hashCodeEquals() {
+        Team t1 = new Team();
+        t1.setName("foo");
+        t1.addMember("bar");
+        Team t2 = new Team();
+        t2.setName("foo");
+        t2.addMember("bar");
+        assertEquals(t1.hashCode(), t2.hashCode());
+    }
+
+    @Test
+    public void hashCodeEquivalentMutation() {
+        Team t = new Team();
+        int result = t.hashCode();
+        int expectedResult = 1;
+        assertEquals(expectedResult, result);
+    }
 }
